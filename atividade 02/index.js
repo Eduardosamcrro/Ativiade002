8659d7ee713e5ea60768da5dc9c90ac38ce7051d
Eduardo de Oliveira cunha <97200080+Eduardosamcrro@users.noreply.github.com>
2023-01-26 20:47:54 -0300

    Add files via upload

---
+++ atividade 02/index.js
@@ -0,0 +1,38 @@
+
+/*Declaraçao de variável
+a variavel pode ser escrita de varias formas, uma dela é
+var a,b,c;
+dessa forma, a variavel esta declarando sem a atribuiçao de valores  
+*/
+/*
+var a=200;
+var b= 498;
+var c=81;
+var d=a+b*4-c/7;
+console.log(d);
+console.log(c);
+*/
+var nome, sobrenome, nomeCompleto;
+nome = "Eduardo";
+sobrenome = "Oliveira";
+nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto)
+//document.getElementById("texto").innerHTML= nomeCompleto;
+
+
+/*const NUNCA É ALTERADO 
+OU SEJA:
+CONST X = 9;
+LOGO APOS SER COLOCADO OUTRO CONST, SEMPRE O PRIMEIRO SERA MANTIDO.
+var É O CORINGA, SEMPRE PODERA SER ALTERADO EX:
+VAR X = 6
+VAR X = 9
+O ULTIMO SEMPRE SERA PEGO
+let sempre sera atribuido somente uma vez, a nao ser q esteja dentro de uma chave ex:
+LET X = 9
+LET X= 10   >> ESSE NAO SERA UTILIZADO
+{
+    VAR X = 7 >> ESSE SERA UTILIZADO
+}
+
+*/
